b3bb451d474670ff2a14c030b4f9c0111a457fb9
mwill2214 <114512002+mwill2214@users.noreply.github.com>
2026-08-19 11:12:40 -0400

    M1T1_Williams

---
+++ main.cpp
@@ -0,0 +1,25 @@
+// One line comment
+/*
+Block comment
+M1T1 - Hello World
+Williams
+8/19/26
+First program - just a smoke test.
+*/
+
+// magic words to start up
+#include <iostream>
+using namespace std;
+
+// main() is a FUNCTION - a verb
+int main()
+{
+    count << "Hello CSC 134!" << endl;
+    // name is a VARIABLE - a noun
+    string name;
+    count << "What's your name? ";
+    cin >> name; // input - reads the name into this variable
+
+    count << "This program is for: " << name << endl;
+    return 0; // no errors
+}
